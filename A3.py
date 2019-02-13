4d19098e09c3fb5c12513618672eee51396abc6f
IPhoneXScams <47580999+IPhoneXScams@users.noreply.github.com>
2019-02-12 17:52:26 -0700

    Add files via upload

---
+++ A3.py
@@ -0,0 +1,71 @@
+import turtle
+import os
+
+#setup screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+#draw border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.down()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+#create the player Turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+playerspeed = 15
+
+#create the enemy
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+enemyspeed = 2
+#move player to the left and right
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x < -280:
+        x = - 280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x > 280:
+        x = 280
+    player.setx(x)
+
+#keyboard bindings
+wn.listen()
+wn.onkey(move_left, "Left")
+wn.onkey(move_right,"Right")
+
+#main game loop
+while True:
+
+    #move the enemy
+    x = enemy.xcor()
+    x+= enemyspeed
+    enemy.setx(x)
+
+
+delay = raw_input("Press enter to finish")
